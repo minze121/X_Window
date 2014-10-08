9448095adac149bb8cf30246a3787673b5453de5
minze121 <dizedong@gmail.com>
2014-10-08 13:36:09 +0800

    Add the ioctl function
    Add the ioctl function
          BTN_LEFT
          BTN_RIGHT

--- vts/vts-drv.c
+++ vts/vts-drv.c
@@ -7,12 +7,67 @@
 #include <linux/device.h>
 #include <linux/miscdevice.h>
 #include <asm/uaccess.h>
+#include <linux/delay.h>
 #include <linux/input.h>
 
+//IOCTL DEFINE
+#define TYPE_MAGIC_NR 'M'
+#define MAX_IOC_NR 0x100
+#define LEFT_CLICK _IOR(TYPE_MAGIC_NR,0,int)
+
+#define VTS_MISCDEV_MINOR	183 
+
 struct input_dev *input_dev;
-#define VTS_MISCDEV_MINOR	183
 
 static int vts_open(struct inode *inode,struct file *filp) {
+	return 0;
+}
+
+static int buf[2];
+//static int vts_ioctl(struct inode * inode, struct file * flip, unsigned int cmd, unsigned long arg)
+static long vts_ioctl(struct file * flip, unsigned int cmd, unsigned long arg)
+{
+	int err = 0;
+
+	//1 check command valid
+	if (!(_IOC_TYPE(cmd) & TYPE_MAGIC_NR))
+		return -EINVAL;
+
+	if (_IOC_NR(cmd) > MAX_IOC_NR)
+		return -EINVAL;
+
+	//2. check param valid
+	if (_IOC_DIR(cmd) & _IOC_READ)
+	{
+		err = !access_ok(VERIFY_WRITE,(void __user *)arg,4);
+	}
+	else if (_IOC_DIR(cmd) & _IOC_WRITE)
+	{
+		err = !access_ok(VERIFY_READ,(void __user *)arg,4);
+	}
+
+	if (err)
+		return -EFAULT;
+//#define BTN_LEFT		0x110
+//#define BTN_RIGHT		0x111
+	//implement the command
+	switch(cmd)
+	{
+	case LEFT_CLICK:
+		copy_from_user(buf, (int *)arg, sizeof(buf));
+		static int count = 0;
+		input_report_key(input_dev, BTN_LEFT, 1);
+		input_sync(input_dev);
+
+		input_report_key(input_dev, BTN_LEFT, 0);
+		input_sync(input_dev);
+
+//		printk("%d LEFT_CLICK %d %d\n",count++,buf[0],buf[1]);
+		break;
+	default:
+		break;
+	}
+
 	return 0;
 }
 
@@ -43,6 +98,8 @@ static struct file_operations vts_fops = {
 	.owner   = THIS_MODULE,
 	.open    = vts_open,
 	.write   = vts_write,
+//	.ioctl   = vts_ioctl,
+//	.unlocked_ioctl   = vts_ioctl,
 	.release = vts_release
 };
 
@@ -65,8 +122,11 @@ static int __init vts_init(void)
 	}
 
 	input_dev->name = "vts";
+
 	input_dev->evbit[0] = BIT_MASK(EV_KEY) | BIT_MASK(EV_SYN)| BIT_MASK(EV_ABS);
 	input_dev->keybit[BIT_WORD(BTN_MOUSE)] = BIT_MASK(BTN_MOUSE);
+//	input_dev->keybit[BIT_WORD(BTN_MOUSE)] = BIT_MASK(BTN_LEFT) |BIT_MASK(BTN_MIDDLE) | BIT_MASK(BTN_RIGHT);
+
 	input_set_abs_params(input_dev, ABS_X,  0, 3200, 0, 0);
 	input_set_abs_params(input_dev, ABS_Y,  0, 1280, 0, 0);
 
@@ -81,6 +141,8 @@ static int __init vts_init(void)
 	if (err) {
 		goto err1;
 	}
+
+	printk("%s OK\n", __func__);
 
 	return 0;
 
@@ -97,6 +159,7 @@ static void __exit vts_exit(void) {
 	input_unregister_device(input_dev);
 	input_free_device(input_dev);
 }
+
 
 module_init(vts_init);
 module_exit(vts_exit);
